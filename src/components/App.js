8996599ca7b63d8db9f268a8ac6e7986e7408d2a
irinamatsiyaka <137830014+irinamatsiyaka@users.noreply.github.com>
2025-11-20 12:54:57 +0300

    Revert "minor fixes"

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Web Frontend</h1>
+      <p>Frontend is running successfully inside Docker!</p>
+      <p>Connected to backend API at <code>http://localhost:8000</code></p>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./styles/App.css";
+import App from "./components/App";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
-   <StrictMode>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-   </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
